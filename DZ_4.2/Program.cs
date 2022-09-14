10cd57a11145dc134af3d3e25d37ba470ea5c13e
Aleksey Podusov <apodusov@gmail.com>
2022-09-14 21:03:12 +0300

    Задача 4.27

---
+++ DZ_4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (number > 0)
+{
+    int digit = number % 10; 
+    sum = digit + sum;
+    number = number / 10;   
+
+}
+Console.WriteLine(sum);
