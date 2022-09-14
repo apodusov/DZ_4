3b918edf3feba9f7f192f8cb48bebbe53d2d2897
Aleksey Podusov <apodusov@gmail.com>
2022-09-14 20:55:55 +0300

    Задача 4.25

---
+++ DZ_4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int num2 = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 0; i < num2; i++)
+{
+    result = result * num1;
+}
+
+Console.WriteLine(result);
